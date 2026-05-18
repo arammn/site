a463b95dd7d2a58622c4e786d1d9b28f5bd0f9ee
arammn <aramm6013@gmail.com>
2026-05-17 21:56:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import asyncio, logging, sys
 from pathlib import Path
 from telegram.ext import ApplicationBuilder, ContextTypes
-from telegram.error import Conflict
 from config import Config
 from database import Database
 from auction import AuctionManager
@@ -54,11 +53,6 @@ class App:
             )
             logger.info("Heartbeat (1h) started")
             await asyncio.Future()
-        except Conflict:
-            logger.error("Conflict: Another bot instance is already running. Shutting down.")
-            await self.application.updater.stop()
-            await self.application.stop()
-            await self.application.shutdown()
         except KeyboardInterrupt:
             pass
         finally:
